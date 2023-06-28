52750fe7a4c0b0d0fc34a4709868880088a17fa5
masteroffluff <131491594+masteroffluff@users.noreply.github.com>
2023-06-28 23:36:04 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -20,43 +20,43 @@ function pAequorFactory(specimenNum, dna){
     specimenNum:specimenNum,
     dna:dna,
     // lets do some !!Science!!
-    mutate:()=>{
-      let randomBaseID = Math.floor(Math.random() * dna.length);
+    mutate(){
+      let randomBaseID = Math.floor(Math.random() * this.dna.length);
       let newBase;
       do{
         const dnaBases = ['A', 'T', 'C', 'G'];
         newBase= dnaBases[Math.floor(Math.random() * 4)];
-      } while (newBase === dna[randomBaseID]);
-      dna[randomBaseID] = newBase;
-      return dna;
+      } while (newBase === this.dna[randomBaseID]);
+      this.dna[randomBaseID] = newBase;
+      return this.dna;
     },
 
     // function takes another instance of the object and iterates thought both of their dna arrays and compares them. it counts the matches. 
-    compareDNA:(pAequor)=>{
+    compareDNA(pAequor){
         let matchCount = 0;
-        for(i=0;i<dna.length;i++){
-          if(dna[i]===pAequor.dna[i]){
+        for(i=0;i<this.dna.length;i++){
+          if(this.dna[i]===pAequor.dna[i]){
             matchCount++
           }
         }
         // divide the number of matches against the length of the dna array and multiply it by 100 to give us the percentage
-        let percentMatch = (matchCount/dna.length)*100
+        let percentMatch = (matchCount/this.dna.length)*100
         // console log as per requirement 
         console.log (`Specimen #${specimenNum} and Specimen #${pAequor.specimenNum} have ${percentMatch}% DNA in common`)
         return percentMatch; // added this for the further challenge relationship building 
     },
     // measures the survivability of the oganism by counting the C and G bases
-    willLikelySurvive: ()=> {
+    willLikelySurvive (){
       let countOfCOrG = 0;
-      dna.forEach(base => {if(base==="C"||base==="G"){countOfCOrG++}}
+      this.dna.forEach(base => {if(base==="C"||base==="G"){countOfCOrG++}}
        );
        // the criteria is over 60% c or g so divide count by dna length and return if it is over 0.6 (60%)
-       return (countOfCOrG/dna.length)>=0.6;     
+       return (countOfCOrG/this.dna.length)>=0.6;     
     },
     // further challenge section
     // iterates through the dna and uses a switch statmenr to swap c fo g and a for t and vice versa
-    complementStrand:()=>{
-      return dna.map(base=>{
+    complementStrand(){
+      return this.dna.map(base=>{
         switch (base){
           case "A": return "T"
           case "T": return "A"
@@ -73,14 +73,15 @@ function pAequorFactory(specimenNum, dna){
 /// generate 30 organisms that will survive for the team
 function make30(){
   let i=0;
+  
   let petriDish=[];
-  while(i<30){
+  while(petriDish.length<30){
     let pAequor = pAequorFactory(i, mockUpStrand());
     // test if the organism will likely survive, add it to the list if not the organism is discarded
     if(pAequor.willLikelySurvive()){
       petriDish.push(pAequor); 
-      i++;
     }
+    i++;
   }
   // return the 30 survivors
   return petriDish
@@ -139,6 +140,7 @@ const sample2 = pAequorFactory (2, ['A', 'A', 'A', 'G', 'G', 'G', 'G', 'G', 'G',
 
 // tests 
 // muation 
+console.log("mutation")
 console.log(sample0.dna)         // returns all as
 console.log (sample0.mutate())  // returns all as but 1
 console.log(sample0.dna)        // returns all as but 1
@@ -153,7 +155,7 @@ console.log(sample1.willLikelySurvive())// should return false
 console.log(sample2.willLikelySurvive())// should return true
 
 
-// tests on wild pAequors with random dna
+// tests on wild pAequors with random dna to make sure this works for reals
 const sample3 = pAequorFactory (3, mockUpStrand())
 const sample4 = pAequorFactory (4, mockUpStrand())
 sample3.compareDNA(sample4) // should give a dfferent answer each run
@@ -169,6 +171,7 @@ console.log (sample3.dna)
 
 // return results for test
 const collectionOf30 = make30();
+collectionOf30.forEach(item=>console.log(item)) // prinitng like this because VSC does not like to print lots of arrays on the console
 
 
 
@@ -186,7 +189,7 @@ findMostRelated(collectionOf30);
 
 
 // tests 100 times against non-viable to see if we are getting a spread of results highest i saw was 80% lowest 53.333333333333336%.
-// commented out for now as as is will fill the log and obsure the other results. 
+// commented out for now as as is will fill the console and obsure the other results. 
 // for best results comment out the comment.log in compareDNA in pAequorFactory . 
 /* for(let i = 0; i<100;i++){
   let coll = [];
